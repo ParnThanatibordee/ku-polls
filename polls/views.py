922aa4cc6c0082199a92cdfa03f59141856e8ad5
ParnThanatibordee <parn8946@gmail.com>
2021-10-25 15:39:53 +0700

    refactor vote

--- polls/views.py
+++ polls/views.py
@@ -46,7 +46,8 @@ def vote(request, question_id):
     """Vote method."""
     question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+        choice_id = request.POST['choice']
+        selected_choice = question.choice_set.get(pk=choice_id)
     except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
         return render(request, 'polls/detail.html', {
