65d9353cf65e2807348ec9a10c2e999c43e8f0f3
ParnThanatibordee <parn8946@gmail.com>
2021-10-26 13:03:39 +0700

    change status code in test

--- polls/tests.py
+++ polls/tests.py
@@ -150,7 +150,7 @@ class QuestionDetailViewTests(TestCase):
         future_question = create_question(question_text='Future question.', days=5)
         url = reverse('polls:detail', args=(future_question.id,))
         response = self.client.get(url)
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
     def test_past_question(self):
         """The detail view of a question with a pub_date in the past displays the question's text."""
